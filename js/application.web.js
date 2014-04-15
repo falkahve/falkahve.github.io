d8ee88bf11d12c64a01e7119f45ba0d8425540aa
soruonline <soruonline@yahoo.com>
2014-04-15 09:44:14 +0300

    Update application.web.js

--- js/application.web.js
+++ js/application.web.js
@@ -1,52 +1,20 @@
-function loadScripts(array,callback){
-    var loader = function(src,handler){
-        var script = document.createElement("script");
-        script.src = src;
-        script.onload = script.onreadystatechange = function(){
-        script.onreadystatechange = script.onload = null;
-        	handler();
-        }
-        var head = document.getElementsByTagName("head")[0];
-        (head || document.body).appendChild( script );
-    };
-    (function(){
-        if(array.length!=0){
-        	loader(array.shift(),arguments.callee);
-        }else{
-        	callback && callback();
-        }
-    })();
+if (typeof jQuery !== 'undefined') {
+	(function($) {
+		$('#spinner').ajaxStart(function() {
+			$(this).fadeIn();
+		}).ajaxStop(function() {
+			$(this).fadeOut();
+		});
+	})(jQuery);
 }
 
-loadScripts([
-	"http://code.jquery.com/jquery-1.10.2.min.js",
-	"http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js",
-   	"http://cdn.soruonline.com/js/jquery.fittext.js",
-	"http://cdn.soruonline.com/js/highlight.pack.js",
-	"http://cdn.soruonline.com/js/jasny-bootstrap.min.js",
-	"http://cdn.soruonline.com/js/docs.min.js",
-	"http://cdn.soruonline.com/js/bootstrap-maxlength.min.js"
-],function(){
-    console.info('All things are loaded');
-    
-    if (typeof jQuery !== 'undefined') {
-		(function($) {
-			$('#spinner').ajaxStart(function() {
-				$(this).fadeIn();
-			}).ajaxStop(function() {
-				$(this).fadeOut();
-			});
-		})(jQuery);
-	}
-
-	$(document).ready(function() {
- 		$('textarea[id^="textarea_"]').maxlength({
-			alwaysShow: true,
-	     		warningClass: 'label label-warning',
-	     		limitReachedClass: 'label label-success'
+$(document).ready(function() {
+ 	$('textarea[id^="textarea_"]').maxlength({
+		alwaysShow: true,
+     		warningClass: 'label label-warning',
+     		limitReachedClass: 'label label-success'
      	});
 });
-    
-});
+
 
 
