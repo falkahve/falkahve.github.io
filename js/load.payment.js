94856b9130a21b2afe87341a57f7a94c87e6c9c7
soruonline <soruonline@yahoo.com>
2014-04-15 10:27:42 +0300

    Create load.payment.js

---
+++ js/load.payment.js
@@ -0,0 +1,28 @@
+function loadScripts(array,callback){
+    var loader = function(src,handler){
+        var script = document.createElement("script");
+        script.src = src;
+        script.onload = script.onreadystatechange = function(){
+        script.onreadystatechange = script.onload = null;
+        	handler();
+        }
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(script, s);
+        //(head || document.body).appendChild( script );
+    };
+    (function(){
+        if(array.length!=0){
+        	loader(array.shift(),arguments.callee);
+        }else{
+        	callback && callback();
+        }
+    })();
+}
+
+loadScripts([
+	"http://code.jquery.com/jquery-1.10.2.min.js",
+	"http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js",
+	"http://cdn.soruonline.com/js/application.payment.js"
+],function(){
+    console.info('loaded.');
+});
