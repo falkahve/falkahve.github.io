d3bef6946b311dc5979fd4771e41c98fc409bbdb
soruonline <soruonline@yahoo.com>
2014-04-18 02:46:03 +0300

    Update chat.web.js

--- js/chat.web.js
+++ js/chat.web.js
@@ -1,6 +1,3 @@
-<!-- Envolve Chat -->
-<script type="text/javascript">
 var envoSn=161980;
 var envProtoType = (("https:" == document.location.protocol) ? "https://" : "http://");
 document.write(unescape("%3Cscript src='" + envProtoType + "d.envolve.com/env.nocache.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
