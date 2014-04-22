c97642d2ce40b8cdb505a87884c81dcecbb5641c
Emrah SARI <emrahsari@msn.com>
2014-04-22 07:37:37 +0300

    chat

--- js/chat.web.js
+++ js/chat.web.js
@@ -1,3 +1,3 @@
-var envoSn=161980;
+var envoSn=162505;
 var envProtoType = (("https:" == document.location.protocol) ? "https://" : "http://");
 document.write(unescape("%3Cscript src='" + envProtoType + "d.envolve.com/env.nocache.js' type='text/javascript'%3E%3C/script%3E"));
