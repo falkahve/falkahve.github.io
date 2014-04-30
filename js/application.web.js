e7c324951dabbd2d7f992ae74251e8317049a1c8
Emrah SARI <emrahsari@msn.com>
2014-04-30 13:17:52 +0300

    no message

--- js/application.web.js
+++ js/application.web.js
@@ -9,7 +9,7 @@ if (typeof jQuery !== 'undefined') {
 }
 
 
-$.backstretch("http://cdn.soruonline.com/img/bg.jpg", {speed: 500});
+$.backstretch("http://cdn.soruonline.com/img/bg1.jpg", {speed: 500});
 $('.cropme').simpleCropper();
 
 function show(obj) {
