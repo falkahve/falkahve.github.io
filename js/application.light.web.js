381bd2c1e7eac14a646f5f32eaae533dffcea66a
Emrah SARI <emrahsari@msn.com>
2014-05-08 20:58:06 +0300

    no message

--- js/application.light.web.js
+++ js/application.light.web.js
@@ -47,43 +47,43 @@ var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
   'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
 ];
 
-// $('.tagsinput').tagsinput({
-//   typeahead: {
-//     source: substringMatcher(states)
-//   }
-// });
-
-var elt = $('input');
-
-elt.tagsinput({
-  tagClass: function(item) {
-    switch (item.continent) {
-      case 'Europe'   : return 'label label-primary';
-      case 'America'  : return 'label label-danger label-important';
-      case 'Australia': return 'label label-success';
-      case 'Africa'   : return 'label label-default';
-      case 'Asia'     : return 'label label-warning';
-    }
-  },
-  itemValue: 'value',
-  itemText: 'text'
+$('input').tagsinput({
+  typeahead: {
+    source: substringMatcher(states)
+  }
 });
 
-elt.tagsinput('add', { "value": 1 , "text": "Amsterdam"   , "continent": "Europe"    });
-elt.tagsinput('add', { "value": 4 , "text": "Washington"  , "continent": "America"   });
-elt.tagsinput('add', { "value": 7 , "text": "Sydney"      , "continent": "Australia" });
-elt.tagsinput('add', { "value": 10, "text": "Beijing"     , "continent": "Asia"      });
-elt.tagsinput('add', { "value": 13, "text": "Cairo"       , "continent": "Africa"    });
+// var elt = $('input');
 
-elt.tagsinput('input').typeahead({
-  valueKey: 'text',
-  source: substringMatcher(states),
-  template: '<p>{{text}}</p>',                                       
-  engine: Hogan
-}).bind('typeahead:selected', $.proxy(function (obj, datum) {  
-	this.tagsinput('add', datum);
-	this.tagsinput('input').typeahead('setQuery', '');
-}, elt));
+// elt.tagsinput({
+//   tagClass: function(item) {
+//     switch (item.continent) {
+//       case 'Europe'   : return 'label label-primary';
+//       case 'America'  : return 'label label-danger label-important';
+//       case 'Australia': return 'label label-success';
+//       case 'Africa'   : return 'label label-default';
+//       case 'Asia'     : return 'label label-warning';
+//     }
+//   },
+//   itemValue: 'value',
+//   itemText: 'text'
+// });
+
+// elt.tagsinput('add', { "value": 1 , "text": "Amsterdam"   , "continent": "Europe"    });
+// elt.tagsinput('add', { "value": 4 , "text": "Washington"  , "continent": "America"   });
+// elt.tagsinput('add', { "value": 7 , "text": "Sydney"      , "continent": "Australia" });
+// elt.tagsinput('add', { "value": 10, "text": "Beijing"     , "continent": "Asia"      });
+// elt.tagsinput('add', { "value": 13, "text": "Cairo"       , "continent": "Africa"    });
+
+// elt.tagsinput('input').typeahead({
+//   valueKey: 'text',
+//   source: substringMatcher(states),
+//   template: '<p>{{text}}</p>',                                       
+//   engine: Hogan
+// }).bind('typeahead:selected', $.proxy(function (obj, datum) {  
+// 	this.tagsinput('add', datum);
+// 	this.tagsinput('input').typeahead('setQuery', '');
+// }, elt));
 
 function show(obj) {
 		    var el = document.getElementById(obj);
