ab06a65f13d01aee2e9e143d51940c758af4d086
Emrah SARI <emrahsari@msn.com>
2014-05-08 20:53:16 +0300

    no message

--- js/application.light.web.js
+++ js/application.light.web.js
@@ -8,44 +8,44 @@ if (typeof jQuery !== 'undefined') {
 	})(jQuery);
 }
 
-// $.backstretch("http://cdn.soruonline.com/img/bg1.jpg", {speed: 500});
-// $('.cropme').simpleCropper();
+$.backstretch("http://cdn.soruonline.com/img/bg1.jpg", {speed: 500});
+$('.cropme').simpleCropper();
 
-// var substringMatcher = function(strs) {
-//   return function findMatches(q, cb) {
-//     var matches, substringRegex;
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
  
-//     // an array that will be populated with substring matches
-//     matches = [];
+    // an array that will be populated with substring matches
+    matches = [];
  
-//     // regex used to determine if a string contains the substring `q`
-//     substrRegex = new RegExp(q, 'i');
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
  
-//     // iterate through the pool of strings and for any string that
-//     // contains the substring `q`, add it to the `matches` array
-//     $.each(strs, function(i, str) {
-//       if (substrRegex.test(str)) {
-//         // the typeahead jQuery plugin expects suggestions to a
-//         // JavaScript object, refer to typeahead docs for more info
-//         matches.push({ value: str });
-//       }
-//     });
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
  
-//     cb(matches);
-//   };
-// };
+    cb(matches);
+  };
+};
 
 
-// var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-//   'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-//   'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-//   'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-//   'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-//   'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-//   'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-//   'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-//   'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-// ];
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
 
 // $('.tagsinput').tagsinput({
 //   typeahead: {
@@ -77,7 +77,7 @@ elt.tagsinput('add', { "value": 13, "text": "Cairo"       , "continent": "Africa
 
 elt.tagsinput('input').typeahead({
   valueKey: 'text',
-  prefetch: 'js/cities.json',
+  source: substringMatcher(states),
   template: '<p>{{text}}</p>',                                       
   engine: Hogan
 }).bind('typeahead:selected', $.proxy(function (obj, datum) {  
