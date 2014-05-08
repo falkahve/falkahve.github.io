2735ba16aa4d2fc3c779c8b763e6c43b7b899730
Emrah SARI <emrahsari@msn.com>
2014-05-08 19:59:44 +0300

    no message

--- js/application.light.web.js
+++ js/application.light.web.js
@@ -11,6 +11,31 @@ if (typeof jQuery !== 'undefined') {
 // $.backstretch("http://cdn.soruonline.com/img/bg1.jpg", {speed: 500});
 // $('.cropme').simpleCropper();
 
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+ 
+    // an array that will be populated with substring matches
+    matches = [];
+ 
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+ 
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
+ 
+    cb(matches);
+  };
+};
+
+
 var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
   'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
   'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
